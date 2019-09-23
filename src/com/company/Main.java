6b360b699a648c431b53025759e5b6bdd319a819
Ankit Amonkar <ankit.amonkar@gmail.com>
2019-09-23 12:31:28 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void swap() {
+        int a = (int) (Math.random() * 10);
+    }
+}
